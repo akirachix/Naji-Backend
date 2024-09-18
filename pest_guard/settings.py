efb5bdfe65633134b9566d88a14e8862fed3faa5
Faith-wanjiru <wanjirufaithwanyoike@gmail.com>
2024-09-18 19:44:15 +0300

    added changes

--- pest_guard/settings.py
+++ pest_guard/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN")
+AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID")
+AUTH0_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET")
+REDIRECT_URI = 'http://localhost:8000/auth/callback'
+REDIRECT_URI= 'http://localhost:8000/auth/'

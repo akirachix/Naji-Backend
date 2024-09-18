f84b11b2df02fe8f7ccc21e989e03ef6a4834c1e
Faith-wanjiru <wanjirufaithwanyoike@gmail.com>
2024-09-18 14:46:39 +0300

    added changes

--- pest_guard/pest_guard/settings.py
+++ pest_guard/pest_guard/settings.py
@@ -164,9 +164,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN")
-AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID")
-AUTH0_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET")
+AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN", '')
+AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID", '')
+AUTH0_CLIENT_SECRET = os.environ.get("AUTH0_CLIENT_SECRET", '')
 REDIRECT_URI=  os.environ.get("REDIRECT_URI", '')
 
 

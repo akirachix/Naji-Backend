ed926753dc9b99548247c5da1a89e19f1e74ce9f
Faith-wanjiru <wanjirufaithwanyoike@gmail.com>
2024-09-19 11:49:14 +0300

    commit changes

--- api/views.py
+++ api/views.py
@@ -221,3 +221,5 @@ class RecommendListView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+

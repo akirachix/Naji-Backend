f703116d0cf9262b441b1edac09d5de2b112adf3
Faith-wanjiru <wanjirufaithwanyoike@gmail.com>
2024-09-19 10:49:47 +0300

    added changes to farmer

--- farmer/models.py
+++ farmer/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Farmer(models.Model):
     farmer_id = models.AutoField(primary_key=True)  
     farmer_name = models.CharField(max_length=20)

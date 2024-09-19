a89e33d99f3ea6fb05c253e37a63bf64e45e0283
Faith-wanjiru <wanjirufaithwanyoike@gmail.com>
2024-09-19 14:44:00 +0300

    added changes

--- pest_guard/urls.py
+++ pest_guard/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('api/', include('api.urls')),
     path('auth/',include('authentication.urls')),
 

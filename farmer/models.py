45bc1fed234c81e39983edffe873294af8ee9af9
Faith-wanjiru <wanjirufaithwanyoike@gmail.com>
2024-09-25 13:38:52 +0300

    added changes in farmer model

--- farmer/models.py
+++ farmer/models.py
@@ -1,13 +1,16 @@
+
+
 from django.db import models
 
 class Farmer(models.Model):
-    farmer_id = models.PositiveSmallIntegerField(primary_key=True)  
+    farmer_id = models.AutoField(primary_key=True)  
     farmer_name = models.CharField(max_length=20)
     farmer_county = models.CharField(max_length=100)
     farmer_phone_number = models.CharField(max_length=10)
 
     def __str__(self):
         return f"{self.farmer_name}"
+
 
 
  

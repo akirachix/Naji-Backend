29aef7cf8fab0a900f126f1b29e480b1faed9e84
Esther-Naserian <naseriansamuria@gmail.com>
2024-10-24 22:47:21 +0300

    added changes

--- pestincident/tests.py
+++ pestincident/tests.py
@@ -23,9 +23,7 @@ class PestIncidentModelTest(TestCase):
         self.assertEqual(incident.detection_date, date.today())
 
     def test_pestincident_creation_unhappy_path_missing_leaf_status(self):
-        """
-        Unhappy Path: Missing leaf_status should raise an IntegrityError
-        """
+        
         with self.assertRaises(IntegrityError):
             PestIncident.objects.create(
                 affected_area_percentage=15.0,
@@ -45,9 +43,7 @@ class PestIncidentModelTest(TestCase):
             incident.full_clean()  
 
     def test_pestincident_creation_unhappy_path_missing_confidence_score(self):
-        """
-        Unhappy Path: Missing confidence_score should raise an IntegrityError
-        """
+       
         with self.assertRaises(IntegrityError):
             PestIncident.objects.create(
                 leaf_status="Infected",
